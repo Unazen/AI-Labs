823d7ca6f0441f01166eb6a381b093df99c2fe26
Unazen <unazen.sec@protonmail.com>
2026-03-29 13:11:39 +0300

    добавлены необходимые действия для каждой из задач

--- classification_lab3.py
+++ classification_lab3.py
@@ -1,10 +1,37 @@
 import pandas as pd
 from sklearn.metrics import roc_curve
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 
+#загрузка датасета
 df=pd.read_csv("processed_lab1_classification.csv")
+print(df.head())
 
+#разделение на цель и параметры
+X=df.drop("Personality", axis=1)
+y=df["Personality"]
+
+# разделение на train test
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.4,random_state=42)
+
+#модель
 y_proba = ml_model.predict_proba(X_test)
 print(ml_model.classes_)
 
 
 fpr, tpr, thresholds = roc_curve(y_test, y_proba[:, 1])
+
+
+#Оценка классификационной модели 
+accuracy = accuracy_score(y_test, y_pred_test)
+
+#matrix
+cm = confusion_matrix(y_test, y_pred_test)
+
+#report
+report = classification_report(y_test, y_pred_test)
+
+#результаты
+print("\nТочность:", accuracy)
+print("\nConfusion matrix",cm )
+print("\nОтчет: ", report)
--- regression_lab3.py
+++ regression_lab3.py
@@ -1,4 +1,29 @@
 import pandas as pd
-from sklearn.model_selection import 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import 
+from sklearn.metrics import mean_squared_error, root_mean_squared_error, mean_absolute_error,r2_score 
 
 df = pd.read_csv("prosseced_regerssion.csv")
+print(df.head())
+
+#разделение на задачу и цели
+X = df.drop("final_grade",axis=1)
+y=df["final_grade"]
+
+#разделение на train/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+
+#модель
+
+
+#оценка
+MSE = mean_squared_error(y_test, y_pred_test)
+RMSE = root_mean_squared_error(y_test, y_pred_test)
+MAE = mean_absolute_error(y_test, y_pred_test)
+r2 = r2_score(y_test, y_pred_test)
+
+#результаты
+print("Mean Square Error, среднеквадратичная ошибка",MSE)
+print("Root MSE, корень среднеквадратичной ошибки",RMSE)
+print("Mean Absolute Error, средняя абсолютная ошибка",MAE)
+print("r^2",r2)
