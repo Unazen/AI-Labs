01e4704e97cbf65de9efa015ec04cb6eb96fd69c
Unazen <unazen.sec@protonmail.com>
2026-04-12 16:00:38 +0300

    добавление Gradient boosting classifier

--- classification_AB_GBC_lab4.py
+++ classification_AB_GBC_lab4.py
@@ -64,3 +64,46 @@ plt.savefig("ROC-curve-AB.png")
 plt.show()
 
 #модель GBC
+
+GBC = GradientBoostingClassifier()
+GBC.fit(X_train,y_train)
+y_pred_test_gbc = GBC.predict(X_test)
+
+#Оценка классификационной модели 
+accuracy = accuracy_score(y_test, y_pred_test_gbc)
+#matrix
+cm = confusion_matrix(y_test, y_pred_test_gbc)
+#report
+report = classification_report(y_test, y_pred_test_gbc)
+#результаты
+print("\nТочность:", accuracy)
+print("\nConfusion matrix",cm )
+print("\nОтчет: ", report)
+
+#работа с ROC кривой (GBC)
+#вероятности (матрица 16 колонок)
+y_proba = GBC.predict_proba(X_test)
+print(GBC.classes_)
+
+# 2. Выбираем класс, который хотим проверить (например, под индексом 0)
+class_index = 0
+class_name = GBC.classes_[class_index]
+
+# 3. Создаем временную метку: 1 если это наш класс, 0 если любой другой из 15
+y_test_binary = (y_test == class_name).astype(int)
+
+# 4. Считаем ROC
+fpr, tpr, _ = roc_curve(y_test_binary, y_proba[:, class_index])
+roc_auc = auc(fpr, tpr)
+score = roc_auc_score(y_test, y_proba, multi_class='ovr')
+
+#Визуализация ROC-кривой
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC-кривая')
+plt.legend(loc="lower right")
+plt.savefig("ROC-curve-GBC.png")
+plt.show()
